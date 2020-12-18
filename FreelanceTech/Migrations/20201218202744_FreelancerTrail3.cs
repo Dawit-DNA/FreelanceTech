70f2802a8215d0a4272d2522a673b02f0463215c
Dawit <dnaishere@gmail.com>
2020-12-18 23:44:46 +0300

    Migration added

---
+++ FreelanceTech/Migrations/20201218202744_FreelancerTrail3.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FreelanceTech.Migrations
+{
+    public partial class FreelancerTrail3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "level",
+                table: "Expertise",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "status",
+                table: "Expertise",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "JobSkill",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    jobId = table.Column<string>(nullable: true),
+                    skill = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobSkill", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_JobSkill_Job_jobId",
+                        column: x => x.jobId,
+                        principalTable: "Job",
+                        principalColumn: "jobId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobSkill_jobId",
+                table: "JobSkill",
+                column: "jobId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobSkill");
+
+            migrationBuilder.DropColumn(
+                name: "level",
+                table: "Expertise");
+
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "Expertise");
+        }
+    }
+}
